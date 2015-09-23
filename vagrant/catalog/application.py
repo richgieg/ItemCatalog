cc2f4dbf1082c23f1369d9ee7a8df466ef139063
Richard Gieg <richgieg@users.noreply.github.com>
2015-09-23 12:14:57 -0700

    Initialize templates

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,35 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def show_main():
-    return "This is the main page"
+    return render_template('show_main.html')
 
 
 @app.route('/<string:category_id>/')
 def show_items(category_id):
-    return "This will display category '%s'" % category_id
+    return render_template('show_items.html', category_id = category_id)
 
 
 @app.route('/<string:category_id>/<string:item_id>')
 def show_item(category_id, item_id):
-    return "This will display item '%s' from category '%s'" % (item_id, category_id)
+    return render_template('show_item.html', category_id = category_id,
+                           item_id = item_id)
 
 
 @app.route('/items/new')
 def new_item():
-    return "This is the new item page"
+    return render_template('new_item.html')
 
 
 @app.route('/items/<string:item_id>/edit')
 def edit_item(item_id):
-    return "This is the edit page for '%s'" % item_id
+    return render_template('edit_item.html', item_id = item_id)
 
 
 @app.route('/items/<string:item_id>/delete')
 def delete_item(item_id):
-    return "This is the delete page for '%s'" % item_id
+    return render_template('delete_item.html', item_id = item_id)
 
 
 if __name__ == '__main__':
